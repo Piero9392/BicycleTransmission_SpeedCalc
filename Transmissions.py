7610e016bafdd81f0723d654fc49a9d359af6a51
Igor Kolesnykov <132067668+Piero9392@users.noreply.github.com>
2024-11-03 02:24:40 +0200

    Improved version of the code, with cleaner file handling, optimized functions, and more readable formatting for output
    I’ve combined the belt and chain functions, added a drive_type parameter to differentiate between belt and chain drives, and used with open(...) to ensure files are properly closed. I also structured the output file more cleanly and improved error handling.

--- Transmissions.py
+++ Transmissions.py
@@ -38,112 +38,77 @@ transmission = {
         'front_cog': 34,
         'cassette': (51, 45, 39, 33, 28, 24, 21, 18, 16, 14, 12, 10)}
 }
-# Speed function for transmission with belt
-def belt(name, gear_ratio, front_cog, rear_cog, wheel_size, cadence):
-    print(f'{name} - Total range: {round(gear_ratio[-1] / gear_ratio[0] * 100)}%')
-    print('Gear | Ratio | Speed')
-    file = open('Data/Text.txt', 'a')
-    file.write(f'{name} (Total range: {round(gear_ratio[-1] / gear_ratio[0] * 100)}%)\n')
-    file.write('Gear | Ratio | Speed\n')
-    for i in range(0, len(gear_ratio)):
-        speed = gear_ratio[i] * float(wheel_size) * 0.0000254 * 3.14159 * float(cadence) * 60 * (front_cog / rear_cog)
-        print(f'{i+1} | {"%.2f" % gear_ratio[i]} | {round(speed, 1)} km/h')
-        file = open('Data/Text.txt', 'a')
-        file.write(f'{i+1} | {"%.2f" % gear_ratio[i]} | {round(speed, 1)} km/h\n')
-        file.close()
-    file = open('Data/Text.txt', 'a')
-    file.write('---------------------\n')
+
+# Speed calculation function
+def calculate_speed(name, gear_ratio, wheel_size, cadence, front_cog=None, rear_cog=None, drive_type='chain'):
+    range_percent = round((gear_ratio[-1] / gear_ratio[0]) * 100)
+    header = f"{name} - Total range: {range_percent}%\nGear | Ratio | Speed (km/h)"
+    print(header)
+    
+    with open('Data/Text.txt', 'a') as file:
+        file.write(header + '\n')
+        
+        for i, ratio in enumerate(gear_ratio):
+            if drive_type == 'belt':
+                speed = ratio * wheel_size * 0.0000254 * 3.14159 * cadence * 60 * (front_cog / rear_cog)
+            else:  # chain drive
+                speed = ratio * wheel_size * 0.0000254 * 3.14159 * cadence * 60
+            
+            output_line = f"{i + 1} | {ratio:.2f} | {round(speed, 1)} km/h"
+            print(output_line)
+            file.write(output_line + '\n')
+        
+        file.write('---------------------\n')
     print('---------------------')
-# Speed function for transmission with derailleur
-def chain(name, gear_ratio, wheel_size, cadence):
-    print(f'{name} - Total range: {round(gear_ratio[-1] / gear_ratio[0] * 100)}%')
-    print('Gear | Ratio | Speed')
-    file = open('Data/Text.txt', 'a')
-    file.write(f'{name} (Total range: {round(gear_ratio[-1] / gear_ratio[0] * 100)}%)\n')
-    file.write('Gear | Ratio | Speed\n')
-    for i in range(0, len(gear_ratio)):
-        speed = gear_ratio[i] * float(wheel_size) * 0.0000254 * 3.14159 * float(cadence) * 60
-        print(f'{i+1} | {"%.2f" % gear_ratio[i]} | {round(speed, 1)} km/h')
-        file = open('Data/Text.txt', 'a')
-        file.write(f'{i+1} | {"%.2f" % gear_ratio[i]} | {round(speed, 1)} km/h\n')
-        file.close()
-    file = open('Data/Text.txt', 'a')
-    file.write('---------------------\n')
-    file.close()
-    print('---------------------')
-# Input data of 'wheel size' and 'cadence'
+
+# User input for wheel size and cadence
 wheel_size = float(input('Enter wheel size (inch): '))
 cadence = int(input('Enter cadence (rpm): '))
 print('---------------------')
-# Range for wheel size of 'Transmission list' and 'cadence' from 0 to 120 rpm
-if wheel_size == transmission['Wheel size'][0] or wheel_size == transmission['Wheel size'][1] or wheel_size == transmission['Wheel size'][2] or wheel_size == transmission['Wheel size'][3]:
-    if cadence > 0 and cadence <= 120:
-        # Write data file
-        file = open('Data/Text.txt', 'w')
-        file.write('Wheel size: '+ str(wheel_size) + 'inch\n')
-        file.write('Cadence: '+ str(cadence) + 'rpm\n')
-        file.write('--------------------\n')
-        file.close()
-        # Result for Pinion P1.18 with Gates Carbon Drive
-        name = transmission['Pinion P1.18']['name']
-        gear_ratio = transmission['Pinion P1.18']['gear_ratio']
-        front_cog = transmission['Pinion P1.18']['front_cog']
-        rear_cog = transmission['Pinion P1.18']['rear_cog']
-        belt(name, gear_ratio, front_cog, rear_cog, wheel_size, cadence)
-        # Result for Pinion C1.18 with Gates Carbon Drive
-        name = transmission['Pinion C1.12']['name']
-        gear_ratio = transmission['Pinion C1.12']['gear_ratio']
-        front_cog = transmission['Pinion C1.12']['front_cog']
-        rear_cog = transmission['Pinion C1.12']['rear_cog']
-        belt(name, gear_ratio, front_cog, rear_cog, wheel_size, cadence)
-        # Result for Rohloff 500/14 with Gates Carbon Drive
-        name = transmission['Rohloff']['name']
-        gear_ratio = transmission['Rohloff']['gear_ratio']
-        front_cog = transmission['Rohloff']['front_cog']
-        rear_cog = transmission['Rohloff']['rear_cog']
-        belt(name, gear_ratio, front_cog, rear_cog, wheel_size, cadence)
-        # Result for Shimano Alfine 11 with Gates Carbon Drive
-        name = transmission['Shimano Alfine 11']['name']
-        gear_ratio = transmission['Shimano Alfine 11']['gear_ratio']
-        front_cog = transmission['Shimano Alfine 11']['front_cog']
-        rear_cog = transmission['Shimano Alfine 11']['rear_cog']
-        belt(name, gear_ratio, front_cog, rear_cog, wheel_size, cadence)
-        # Result for Shimano Deore 3x10
-        name = transmission['Shimano 3x10']['name']
-        gear_ratio_1 = [transmission['Shimano 3x10']['front_cog'][0] / i for i in transmission['Shimano 3x10']['cassette'][0:4]]
-        gear_ratio_2 = [transmission['Shimano 3x10']['front_cog'][1] / i for i in transmission['Shimano 3x10']['cassette'][3:7]]
-        gear_ratio_3 = [transmission['Shimano 3x10']['front_cog'][2] / i for i in transmission['Shimano 3x10']['cassette'][6:]]
-        gear_ratio = gear_ratio_1 + gear_ratio_2 + gear_ratio_3
-        chain(name, gear_ratio, wheel_size, cadence)
-        # Result for Shimano  GRX 2x11
-        name = transmission['Shimano 2x11']['name']
-        gear_ratio_1 = [transmission['Shimano 2x11']['front_cog'][0] / i for i in transmission['Shimano 2x11']['cassette'][0:7]]
-        gear_ratio_2 = [transmission['Shimano 2x11']['front_cog'][1] / i for i in transmission['Shimano 2x11']['cassette'][4:]]
-        gear_ratio = gear_ratio_1 + gear_ratio_2
-        chain(name, gear_ratio, wheel_size, cadence)
-        # Result for Shimano GRX 1x11
-        name = transmission['Shimano 1x11']['name']
-        gear_ratio = [transmission['Shimano 1x11']['front_cog'] / i for i in transmission['Shimano 1x11']['cassette']]
-        chain(name, gear_ratio, wheel_size, cadence)
-        # Result for Shimano Deore XT 1x12
-        name = transmission['Shimano 1x12']['name']
-        gear_ratio = [transmission['Shimano 1x12']['front_cog'] / i for i in transmission['Shimano 1x12']['cassette']]
-        chain(name, gear_ratio, wheel_size, cadence)
-    # If 'wheelsize' is true and cadence is out of range (0 - 120rpm):
-    else:
-        print('Non-existent value of cadence!\nCadence should be more 0 and less than 120rpm')
-        file = open('Data/Text.txt', 'a')
-        file.write('Wheel size: ' + str(wheel_size) + '"\n')
-        file.write('Cadence: '+ str(cadence) + 'rpm\n')
-        file.write('-----------------\n')
-        file.write('Non-existent value of cadence!\nCadence should be more 0 and less than 120rpm\n')
-        file.close()
-# If 'wheelsize' is out of range
+
+# Validate wheel size and cadence
+if wheel_size in transmission['Wheel size'] and 0 < cadence <= 120:
+    with open('Data/Text.txt', 'w') as file:
+        file.write(f'Wheel size: {wheel_size} inch\nCadence: {cadence} rpm\n--------------------\n')
+
+    # Calculate speeds for belt-driven transmissions
+    belt_drives = ['Pinion P1.18', 'Pinion C1.12', 'Rohloff', 'Shimano Alfine 11']
+    for drive in belt_drives:
+        t = transmission[drive]
+        calculate_speed(
+            name=t['name'],
+            gear_ratio=t['gear_ratio'],
+            wheel_size=wheel_size,
+            cadence=cadence,
+            front_cog=t['front_cog'],
+            rear_cog=t['rear_cog'],
+            drive_type='belt'
+        )
+
+    # Calculate speeds for chain-driven transmissions
+    chain_drives = ['Shimano 3x10', 'Shimano 2x11', 'Shimano 1x11', 'Shimano 1x12']
+    for drive in chain_drives:
+        t = transmission[drive]
+        
+        if drive == 'Shimano 3x10':
+            # Combining three gear ratios based on front cog sets
+            gear_ratio = [t['front_cog'][0] / i for i in t['cassette'][0:4]] + \
+                         [t['front_cog'][1] / i for i in t['cassette'][3:7]] + \
+                         [t['front_cog'][2] / i for i in t['cassette'][6:]]
+        elif drive == 'Shimano 2x11':
+            # Combining two gear ratios based on front cog sets
+            gear_ratio = [t['front_cog'][0] / i for i in t['cassette'][0:7]] + \
+                         [t['front_cog'][1] / i for i in t['cassette'][4:]]
+        else:
+            gear_ratio = [t['front_cog'] / i for i in t['cassette']]
+        
+        calculate_speed(name=t['name'], gear_ratio=gear_ratio, wheel_size=wheel_size, cadence=cadence)
 else:
-    print('Non-existent value of wheel size!\nExisting sizes: 26, 27, 28 or 29inch')
-    file = open('Data/Text.txt', 'a')
-    file.write('Wheel size: ' + str(wheel_size) + '"\n')
-    file.write('Cadence: '+ str(cadence) + 'rpm\n')
-    file.write('-----------------\n')
-    file.write('Non-existent value of wheel size!\nExisting sizes: 26, 27, 28 or 29inch\n')
-    file.close()
+    error_message = 'Non-existent value of wheel size or cadence!\n' \
+                    'Wheel size options: 26, 27.5, 28, or 29 inches\n' \
+                    'Cadence should be more than 0 and less than or equal to 120 rpm\n'
+    print(error_message)
+    with open('Data/Text.txt', 'a') as file:
+        file.write(f'Wheel size: {wheel_size}"\nCadence: {cadence} rpm\n')
+        file.write('-----------------\n')
+        file.write(error_message)
